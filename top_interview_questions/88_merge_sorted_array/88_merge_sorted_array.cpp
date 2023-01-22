e539eef849f4f530b303a6af2bb313110c734b58
gianfillipo <gian.angelo@sptech.school>
2023-01-22 01:35:44 +0000

    88_merge_sorted_array changing variable

--- top_interview_questions/88_merge_sorted_array/88_merge_sorted_array.cpp
+++ top_interview_questions/88_merge_sorted_array/88_merge_sorted_array.cpp
@@ -28,7 +28,6 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int move = m + n - 1;
         int p1 = m - 1;
         int p2 = n - 1;
 
@@ -37,10 +36,10 @@ public:
                 break;
 
             if (p1 >= 0 and nums1[p1] > nums2[p2]) 
-                nums1[move] = nums1[p1--];
+                nums1[p] = nums1[p1--];
 
             else
-                nums1[move] = nums2[p2--];
+                nums1[p] = nums2[p2--];
         }
     }
 };
